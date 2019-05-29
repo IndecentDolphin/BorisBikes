39e8684431d429dbcb60eecaf4866e57a016823f
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-29 16:26:30 +0100

    added bike class, added new tests

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -5,3 +5,9 @@ describe DockingStation do
         it {is_expected.to respond_to(:release_bike)}
     end
 end
+
+    describe "releases and object of type Bike when release_bike called" do
+        subject {DockingStation.new.release_bike}
+         it {is_expected.to be_a(Bike)}
+        
+    end
