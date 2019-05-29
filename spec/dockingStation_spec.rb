06962cfe6b737ffa88a6c60b7559022e3797d7c2
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-29 16:45:44 +0100

    refactored tests

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -1,13 +1,11 @@
 require "DockingStation"
 
 describe DockingStation do
-    describe "Docking Station" do
         it {is_expected.to respond_to(:release_bike)}
-    end
-end
 
-    describe "releases and object of type Bike when release_bike called" do
-        subject {DockingStation.new.release_bike}
-         it {is_expected.to be_a(Bike)}
-        
+        it 'releases working bikes' do
+            bike = subject.release_bike
+        expect(bike).to be_working
+        end
+
     end
