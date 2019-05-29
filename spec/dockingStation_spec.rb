0162e4eb16583f765fdee298095387519cbabc1c
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-29 15:09:22 +0100

    added test for release bike on docking station

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -1,8 +1,13 @@
 require "DockingStation"
 
 describe "Docking Station" do
-  it "tries to load the file" do
-  expect(DockingStation.new.class).to eq(DockingStation)
-end
+    
+    it "tries to load the file" do
+      expect(DockingStation.new.class).to eq(DockingStation)
+    end
+
+    it "responds to the method release_bike" do
+        expect(DockingStation.new).to respond_to(release_bike)
+    end
     
 end
