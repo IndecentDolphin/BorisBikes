09aa1a5148a49e898c387f9efca14c8cf8088c4e
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-29 17:30:24 +0100

    added dock method and tests related

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,10 +1,16 @@
-require "bike"
+require_relative './bike.rb'
 class DockingStation
+    attr_reader :docked_bikes
+    def initialize
+        @docked_bikes = []
+    end
+
 
     def release_bike
       return Bike.new
     end
 
-
-
+    def dock(bike)
+        @docked_bikes << bike
+    end
 end
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -1,4 +1,5 @@
 require "DockingStation"
+require "bike"
 
 describe DockingStation do
         it {is_expected.to respond_to(:release_bike)}
@@ -7,5 +8,9 @@ describe DockingStation do
             bike = subject.release_bike
         expect(bike).to be_working
         end
+        
+        it {is_expected.to respond_to(:dock).with(1).argument}
+
+          
 
     end
